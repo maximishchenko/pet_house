a1dd3bab57179bbecfd5efd1e4a055d62a525430
Roznig <timolaev.denis@gmail.com>
2023-12-03 20:51:29 +0300

    fix

--- frontend/web/js/_components.js
+++ frontend/web/js/_components.js
@@ -1,12 +1,9 @@
 import './components/header';
 import './components/swiper';
 import './components/fancy';
-// import './components/calc';
 import './components/catalog';
 import './components/common';
 import './components/sort';
-//import './components/cart';
 import './components/dropzone';
-//import './components/calcNew';
 import './components/calculator';
 import './components/masonry';
--- frontend/web/js/_vendor.js
+++ frontend/web/js/_vendor.js
@@ -1,4 +1,3 @@
 import './vendor/focus-visible.js';
 import './vendor/gridMasonry.js'
-import './vendor/dynamicAdapt.js'
 

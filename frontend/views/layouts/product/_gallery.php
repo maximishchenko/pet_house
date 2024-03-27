570dba9fdde54b0f993d30ef5da8b906742d44a9
Roznig <timolaev.denis@gmail.com>
2024-03-27 10:03:11 +0300

    fix

--- frontend/views/layouts/product/_gallery.php
+++ frontend/views/layouts/product/_gallery.php
@@ -1,7 +1,10 @@
 <?php
 
+use backend\modules\catalog\models\root\Category;
 use backend\modules\catalog\models\root\CategoryUpload;
 use backend\modules\catalog\models\root\Product;
+use common\components\FileType;
+
 ?>
 
 <div class="product-gallary mb-l">
@@ -24,30 +27,34 @@ use backend\modules\catalog\models\root\Product;
   </div>
 
   <!--  TODO Новый слайдер -->
-  <?php if($model->category->uploads): ?>
-  <div class="gallary-photo">
-    <h2 class="gallary-photo__title">Фотогалерея</h2>
-    <div class="swiper gallary-photo__swiper">
-      <div class="swiper-wrapper">
+  <?php if ($model->category->uploads) : ?>
+    <div class="gallary-photo">
+      <h2 class="gallary-photo__title">Фотогалерея</h2>
+      <div class="swiper gallary-photo__swiper">
+        <div class="swiper-wrapper">
 
-        <?php foreach($model->category->uploads as $upload): ?>
-        <div class="swiper-slide gallary-photo__slide">
-          <a class="gallary-photo__link" href="/<?= CategoryUpload::UPLOAD_PATH . $upload->file_path; ?>" data-fancybox="gallary-photo">
-            <img class="gallary-photo__img" src="/<?= CategoryUpload::PREVIEW_UPLOAD_PATH . $upload->file_path; ?>">
-          </a>
-        </div>
+          <?php foreach ($model->category->uploads as $upload) : ?>
+            <div class="swiper-slide gallary-photo__slide">
+              <a class="gallary-photo__link" href="/<?= CategoryUpload::UPLOAD_PATH . $upload->file_path; ?>" data-fancybox="gallary-photo">
+                <?php if ($upload->file_type == FileType::TYPE_IMAGE) : ?>
+                  <img class="gallary-photo__img" src="/<?= CategoryUpload::PREVIEW_UPLOAD_PATH . $upload->file_path; ?>">
+                <?php elseif ($upload->file_type == FileType::TYPE_VIDEO) : ?>
+                  <img class="gallary-photo__img" src="/<?= Category::POSTER_UPLOAD_PATH . $model->category->video_poster; ?>">
+                <?php endif; ?>
+              </a>
+            </div>
 
-        <!-- <div class="swiper-slide gallary-photo__slide gallary-photo__slide--video">
+            <!-- <div class="swiper-slide gallary-photo__slide gallary-photo__slide--video">
           <a class="gallary-photo__link" href="img/photos/v1.mov" data-fancybox="gallary-photo">
             <img class="gallary-photo__img" src="/img/photo/1.jpg" alt="">
           </a>
         </div> -->
-        <?php endforeach; ?>
+          <?php endforeach; ?>
 
+        </div>
+        <div class="swiper-button-next gallary-photo-button-next"></div>
+        <div class="swiper-button-prev gallary-photo-button-prev"></div>
+        <div class="swiper-scrollbar gallary-photo-scrollbar"></div>
       </div>
-      <div class="swiper-button-next gallary-photo-button-next"></div>
-      <div class="swiper-button-prev gallary-photo-button-prev"></div>
-      <div class="swiper-scrollbar gallary-photo-scrollbar"></div>
     </div>
-  </div>
   <?php endif; ?>
